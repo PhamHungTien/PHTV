f9b3d566e85b19a8371b6712d14ed76851e4bedc
Phạm Hùng Tiến <hungtien10a7@gmail.com>
2025-12-28 19:26:28 +0700

    chore: release v1.3.0 with Safe Mode and Ventura support
    - Update appcast.xml with v1.3.0 release notes
    - Update Homebrew formula SHA256 for v1.3.0
    - Minimum macOS lowered from 14.0 to 13.0 (Ventura)

--- homebrew/phtv.rb
+++ homebrew/phtv.rb
@@ -3,7 +3,7 @@
 
 cask "phtv" do
   version "1.3.0"
-  sha256 "76da17899b658d3918c31b2b951ed97f356235c3517db9a2c18d9a791fb64df6"
+  sha256 "499d827aa1bbf8073d160237349c16f84250da6621533930523a4520e2f0b745"
 
   url "https://github.com/PhamHungTien/PHTV/releases/download/v#{version}/PHTV-#{version}.dmg",
       verified: "github.com/PhamHungTien/"
