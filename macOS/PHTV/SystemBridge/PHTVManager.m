381a82fe7f329a90d57ed9f44f2ddcf1d04fa775
Phạm Hùng Tiến <hungtien10a7@gmail.com>
2026-02-20 21:47:27 +0700

    refactor(manager): move Swift bridge selectors into private header

--- macOS/PHTV/SystemBridge/PHTVManager.m
+++ macOS/PHTV/SystemBridge/PHTVManager.m
@@ -10,6 +10,7 @@
 #import <Sparkle/Sparkle.h>
 #import "PHTV-Swift.h"
 #import "PHTVCoreBridge.h"
+#import "PHTVManager+SwiftBridgePrivate.h"
 
 extern void PHTVInit(void);
 
@@ -19,55 +20,6 @@ extern CGEventRef PHTVCallback(CGEventTapProxy proxy,
                                   void *refcon);
 
 extern void PHTVSetCheckSpellingCpp(void) __asm("__Z17vSetCheckSpellingv");
-
-@interface PHTVManager (PHTVSystemServicesBridge)
-
-+(BOOL)phtv_isTCCEntryCorrupt;
-+(BOOL)phtv_autoFixTCCEntryWithError:(NSError **)error;
-+(void)phtv_restartTCCDaemon;
-+(void)phtv_startTCCNotificationListener;
-+(void)phtv_stopTCCNotificationListener;
-+(NSArray *)phtv_getTableCodes;
-+(NSString *)phtv_getApplicationSupportFolder;
-+(NSString *)phtv_getBinaryArchitectures;
-+(NSString *)phtv_getBinaryHash;
-+(BOOL)phtv_hasBinaryChangedSinceLastRun;
-+(BOOL)phtv_checkBinaryIntegrity;
-+(BOOL)phtv_quickConvert;
-+(BOOL)phtv_isSafeModeEnabled;
-+(void)phtv_setSafeModeEnabled:(BOOL)enabled;
-+(void)phtv_clearAXTestFlag;
-+(void)phtv_requestNewSession;
-+(void)phtv_invalidateLayoutCache;
-+(void)phtv_notifyInputMethodChanged;
-+(void)phtv_notifyTableCodeChanged;
-+(void)phtv_notifyActiveAppChanged;
-+(int)phtv_currentLanguage;
-+(void)phtv_setCurrentLanguage:(int)language;
-+(int)phtv_otherLanguageMode;
-+(int)phtv_currentInputType;
-+(void)phtv_setCurrentInputType:(int)inputType;
-+(int)phtv_currentCodeTable;
-+(void)phtv_setCurrentCodeTable:(int)codeTable;
-+(BOOL)phtv_isSmartSwitchKeyEnabled;
-+(BOOL)phtv_isSendKeyStepByStepEnabled;
-+(void)phtv_setSendKeyStepByStepEnabled:(BOOL)enabled;
-+(void)phtv_setUpperCaseExcludedForCurrentApp:(BOOL)excluded;
-+(int)phtv_currentSwitchKeyStatus;
-+(void)phtv_setSwitchKeyStatus:(int)status;
-+(void)phtv_setDockIconRuntimeVisible:(BOOL)visible;
-+(int)phtv_toggleSpellCheckSetting;
-+(int)phtv_toggleAllowConsonantZFWJSetting;
-+(int)phtv_toggleModernOrthographySetting;
-+(int)phtv_toggleQuickTelexSetting;
-+(int)phtv_toggleUpperCaseFirstCharSetting;
-+(int)phtv_toggleAutoRestoreEnglishWordSetting;
-+(NSDictionary<NSString *, NSNumber *> *)phtv_runtimeSettingsSnapshot;
-+(void)phtv_loadEmojiHotkeySettingsFromDefaults;
-+(NSUInteger)phtv_loadRuntimeSettingsFromUserDefaults;
-+(void)phtv_loadDefaultConfig;
-
-@end
 
 @implementation PHTVManager {
 
