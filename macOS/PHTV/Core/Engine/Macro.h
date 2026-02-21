520406224816a8cab390cf115583acb569b4c38d
Phạm Hùng Tiến <hungtien10a7@gmail.com>
2026-02-22 01:34:51 +0700

    chore(macro): remove obsolete C++ macro types

--- macOS/PHTV/Core/Engine/Macro.h
+++ macOS/PHTV/Core/Engine/Macro.h
@@ -10,27 +10,7 @@
 #define Macro_h
 
 #include <vector>
-#include <map>
-#include <string>
 #include "DataType.h"
-
-// Snippet types for dynamic content
-enum SnippetTypeEnum {
-    SNIPPET_STATIC = 0,     // Fixed text (default)
-    SNIPPET_DATE = 1,       // Current date
-    SNIPPET_TIME = 2,       // Current time
-    SNIPPET_DATETIME = 3,   // Date and time
-    SNIPPET_CLIPBOARD = 4,  // Clipboard content
-    SNIPPET_RANDOM = 5,     // Random from list
-    SNIPPET_COUNTER = 6     // Auto-increment number
-};
-
-struct MacroData {
-    std::string macroText; //ex: "ms"
-    std::string macroContent; //ex: "millisecond" or format string for snippets
-    std::vector<Uint32> macroContentCode; //converted of macroContent
-    int snippetType; //0=static, 1=date, 2=time, etc.
-};
 
 /**
  * Call when you need to load macro data from disk
